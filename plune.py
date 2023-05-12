6f13cf0c77681abc80be7c7d777767d906a27c94
Grant Pauker <grantpauker@gmail.com>
2023-05-11 17:51:47 -0700

    Add json log conversion (move folders), cleaned up dump and load functions, add minor styling

--- plune.py
+++ plune.py
@@ -2,20 +2,28 @@
 import argparse
 import json
 import os
-from typing import List, NamedTuple
+from typing import List
 
+import pandas as pd
 import plotly.graph_objects as go
 from dash import Dash, Input, Output, State, callback, dash_table, dcc, html, no_update
-
-from utils import log_parser, odrive_utils
 from google.protobuf import json_format
 
+from generated_protos import header_message_pb2
+from utils import log_parser, odrive_utils
+
+raw_log_dir = "raw_logs"
+json_log_dir = "logs"
+export_dir = "graphs"
 graph_info_file = "graphs.json"
-paths = log_parser.getFilesByExtension("logs", "bin")
+
+with open(graph_info_file, "r") as file:
+    graph_info_json = json.load(file)
+raw_paths = log_parser.getFilesByExtension(raw_log_dir, "bin")
+json_paths = log_parser.getFilesByExtension(json_log_dir, "json")
 
 
 app = Dash(__name__)
-
 
 app.layout = html.Div(
     [
@@ -45,11 +53,28 @@ app.layout = html.Div(
             ],
             type="cube",
         ),
-        dcc.Dropdown(paths, paths[-1], id="file-selection", style={"width": "50%"}),
-        html.Button("Export Graphs", id="export-graphs"),
+        dcc.Dropdown(
+            json_paths,
+            json_paths[-1] if len(json_paths) > 0 else None,
+            id="file-selection",
+            style={"width": "50%"},
+        ),
+        html.Button(
+            "Export Graphs",
+            id="export-graphs",
+            style={
+                "width": "160px",
+                "height": "36px",
+                "cursor": "pointer",
+                "border": "1px solid #cccccc",
+                "border-radius": "5px",
+                "background-color": "white",
+                "color": "black",
+            },
+        ),
         html.Div(
-            "",
-            id="gains",
+            dash_table.DataTable(),
+            id="header",
         ),
         html.Div(
             dash_table.DataTable(),
@@ -63,36 +88,89 @@ app.layout = html.Div(
 )
 
 
-@callback(
-    [
-        Output("export-loading-spinner", "children"),
-        Input("export-graphs", "n_clicks"),
+def exportGraphs() -> None:
+    num_paths = len(json_paths)
+    for n, json_path in enumerate(json_paths):
+        log_data = log_parser.loadJson(json_path, post_process=True)
+        filename = os.path.splitext(os.path.basename(json_path))[0]
+        html_path = os.path.join(export_dir, f"{filename}.html")
+        print(f"Exporting ({n+1}/{num_paths}): {json_path} -> {html_path}")
+        log_parser.dumpLogDataToHTML(html_path, graph_info_json, log_data)
+
+
+def createODriveErrorTable(df: pd.DataFrame) -> dash_table.DataTable:
+    odrive_errors = odrive_utils.getODriveErrors(df)
+    if len(odrive_errors) == 0:
+        return None
+
+    odrive_error_dict = {}
+    for odrive_error in odrive_errors:
+        timestamp_str = f"{odrive_error.timestamp:.03f}"
+        if not timestamp_str in odrive_error_dict:
+            odrive_error_dict[timestamp_str] = "\n".join(odrive_error.names)
+        else:
+            odrive_error_dict[timestamp_str] += "\n" + "\n".join(odrive_error.names)
+
+    odrive_error_rows = [
+        {"timestamp": timestamp, "errors": errors}
+        for timestamp, errors in odrive_error_dict.items()
     ]
+
+    odrive_error_table = dash_table.DataTable(
+        id="datatable",
+        columns=[
+            {"name": "Timestamp", "id": "timestamp"},
+            {"name": "Errors", "id": "errors"},
+        ],
+        data=odrive_error_rows,
+        style_cell={"whiteSpace": "pre-line", "textAlign": "left"},
+        style_header={"backgroundColor": "#ff9999", "fontWeight": "bold"},
+        fill_width=False,
+    )
+    return odrive_error_table
+
+
+def createHeaderTable(header: header_message_pb2.HeaderMessage) -> dash_table.DataTable:
+    table_data = [
+        {"constant": field.name, "value": getattr(header, field.name)}
+        for field in header_message_pb2.HeaderMessage.DESCRIPTOR.fields
+    ]
+    header_table = dash_table.DataTable(
+        columns=[
+            {"name": "Constant", "id": "constant"},
+            {"name": "Value", "id": "value"},
+        ],
+        data=table_data,
+        style_cell={"whiteSpace": "pre-line", "textAlign": "left"},
+        fill_width=False,
+    )
+    return header_table
+
+
+@callback(
+    [Output("export-loading-spinner", "children"), Input("export-graphs", "n_clicks")],
+    prevent_initial_call=True,
 )
 def onExportButtonClicked(n_clicks):
-    if n_clicks == None:
-        return (None,)
-    log_parser.exportGraphsToHTML(paths, graph_info_file, silent=True)
-    log_parser.exportTuningGraphs(paths)
+    exportGraphs()
     return (None,)
 
-import pandas as pd
-import time
+
 @callback(
     [
         Output("title", "children"),
         Output("subtitle", "children"),
         Output("graphs", "children"),
         Output("odrive-errors", "children"),
-        Output("gains", "children"),
+        Output("header", "children"),
         Input("file-selection", "value"),
     ]
 )
-def onFileSelection(path):
+def onLogSelection(path):
     if path == None:
         return no_update
 
-    log_data = log_parser.loadBinary(path)
+    log_data = log_parser.loadJson(path)
     header = log_data.header
     df = log_data.df
 
@@ -102,62 +180,16 @@ def onFileSelection(path):
     if title == "":
         title = "Timestamp Missing"
 
-    graphs = []
-    with open(graph_info_file) as file:
-        graph_json_data = json.load(file)
-        for graph_info in graph_json_data["figures"]:
-            if not set(graph_info["y_axis"]).issubset(df.columns):
-                print(f'Column(s) Missing: Skipping "{graph_info["title"]}" for {path}')
-                continue
+    figures = log_parser.createFigures(graph_info_json, log_data)
+    graphs = [dcc.Graph(figure=fig) for fig in figures]
 
-            traces = [
-                go.Scatter(x=df[graph_info["x_axis"]], y=df[y_axis], name=y_axis)
-                for y_axis in graph_info["y_axis"]
-            ]
+    odrive_error_table = createODriveErrorTable(df)
 
-            fig = go.Figure(traces)
-            fig.update_layout(
-                title=graph_info["title"],
-                xaxis_title=graph_info["x_axis"],
-                showlegend=True,
-                margin=dict(l=20, r=20, t=60, b=20),
-            )
+    header_table = createHeaderTable(header)
 
-            graphs.append(dcc.Graph(figure=fig))
-
-    odrive_errors = odrive_utils.getODriveErrors(df)
-    if len(odrive_errors) != 0:
-        odrive_error_dict = {}
-        for odrive_error in odrive_errors:
-            timestamp_str = f"{odrive_error.timestamp:.03f}"
-            if not timestamp_str in odrive_error_dict:
-                odrive_error_dict[timestamp_str] = "\n".join(odrive_error.names)
-            else:
-                odrive_error_dict[timestamp_str] += "\n" + "\n".join(odrive_error.names)
-
-        odrive_error_rows = [
-            {"timestamp": timestamp, "errors": errors}
-            for timestamp, errors in odrive_error_dict.items()
-        ]
-
-        odrive_error_table = dash_table.DataTable(
-            id="datatable",
-            columns=[
-                {"name": "Timestamp", "id": "timestamp"},
-                {"name": "Errors", "id": "errors"},
-            ],
-            data=odrive_error_rows,
-            style_cell={"whiteSpace": "pre-line", "textAlign": "left"},
-            style_header={"backgroundColor": "#ff9999", "fontWeight": "bold"},
-            fill_width=False,
-        )
-    else:
-        odrive_error_table = None
-
-    return title, path, graphs, [odrive_error_table], json_format.MessageToJson(header)
+    return title, path, graphs, [odrive_error_table], header_table
 
 
-import numpy as np
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="PLotting Utility N Exporter")
 
@@ -165,37 +197,22 @@ if __name__ == "__main__":
         "-e", "--export", action="store_true", help="export all logs to html graphs"
     )
     parser.add_argument(
-        "-t", "--test", action="store_true", help="testing"
+        "-c", "--convert", action="store_true", help="convert all logs to json files"
     )
+    parser.add_argument("-t", "--test", action="store_true", help="testing")
 
     args = parser.parse_args()
 
     if args.export:
-        log_parser.exportGraphsToHTML(paths, graph_info_file)
-    if args.test:
-
-        np.set_printoptions(suppress=True)
-        all_stats = []
-        i = 0
-        for path in paths[-50:]:
-            print(i)
-            orig_size = os.path.getsize(path)/1e6
-            log_data = log_parser.loadBinary(path)
-            header = log_data.header
-            df = log_data.df
-            log_parser.postProcessLogData(log_data)
-            formats, stats = log_parser.dumpLogDataToJson("out", log_data)
-            stats = np.array(stats)
-            stats[:,0] /= orig_size
-            all_stats.append(stats)
-            i += 1
-
-        x = np.array(all_stats)
-        mean_times = np.mean(x[:,:,0], axis=0)
-        mean_memories = np.mean(x[:,:,1], axis=0)
-        print(formats)
-        print(mean_times)
-        print(mean_memories)
-        
+        exportGraphs()
+    elif args.convert:
+        num_paths = len(raw_paths)
+        for n, raw_path in enumerate(raw_paths):
+            log_data = log_parser.loadBinary(raw_path)
+            file_ext = os.path.splitext(raw_path)[1]
+            filename = os.path.splitext(os.path.basename(raw_path))[0]
+            json_path = os.path.join(json_log_dir, f"{filename}.json")
+            print(f"Converting ({n+1}/{num_paths}): {raw_path} -> {json_path}")
+            log_parser.dumpLogDataToJson(json_path, log_data)
     else:
         app.run_server(debug=True)
--- utils/log_parser.py
+++ utils/log_parser.py
@@ -1,17 +1,16 @@
+import io
 import json
 import os
+from enum import IntEnum
 from typing import List, Tuple
-import html
 
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 from google.protobuf import json_format
+from google.protobuf import message as _message
 
 from generated_protos import header_message_pb2, log_message_pb2
-import io
-from google.protobuf import message as _message
-from enum import IntEnum
 
 
 class MessageID(IntEnum):
@@ -30,20 +29,24 @@ class LogData:
         self.df = df
 
 
-def getFilesByExtension(log_dir: str, ext: str) -> List[str]:
-    log_dir_contents = os.listdir(log_dir)
-    txt_paths = []
+def getFilesByExtension(directory: str, ext: str) -> List[str]:
+    log_dir_contents = os.listdir(directory)
+    files = []
     for potential_file in log_dir_contents:
-        potential_file_path = os.path.join(log_dir, potential_file)
+        potential_file_path = os.path.join(directory, potential_file)
         potential_file_ext = os.path.splitext(potential_file)[1]
         if os.path.isfile(potential_file_path) and potential_file_ext == f".{ext}":
-            txt_paths.append(potential_file_path)
-    txt_paths.sort()
-    return txt_paths
+            files.append(potential_file_path)
+    files.sort()
+    return files
 
 
-def filterFilesBySize(paths: List[str], size_kb: int) -> List[str]:
-    return [path for path in paths if os.path.getsize(path) >= size_kb * 1e3]
+def filterFilesBySize(filenames: List[str], size_kb: int) -> List[str]:
+    return [
+        filename
+        for filename in filenames
+        if os.path.getsize(filenames) >= size_kb * 1e3
+    ]
 
 
 def nextDelimitedMessage(
@@ -88,132 +91,52 @@ def loadBinary(path: str) -> LogData:
     return log_data
 
 
-from time import process_time
-import pyarrow as pa
-import pyarrow.parquet as pq
-
-def toParquet(prefix: str, df: pd.DataFrame):
-    start_s = process_time()
-    filename = f"{prefix}.parquet"
-    pq.write_table(pa.Table.from_pandas(df), filename)
-    end_s = process_time()
-    return os.path.getsize(filename)/1e6,end_s-start_s
-
-def toJson(prefix: str, df: pd.DataFrame):
-    start_s = process_time()
-    filename = f"{prefix}.json"
-    df.to_json(filename, orient="split")
-    end_s = process_time()
-    return os.path.getsize(filename)/1e6,end_s-start_s
-
-def toPickle(prefix: str, df: pd.DataFrame):
-    start_s = process_time()
-    filename = f"{prefix}.pickle"
-    df.to_pickle(filename)
-    end_s = process_time()
-    return os.path.getsize(filename)/1e6,end_s-start_s
-
-def toNumpy(prefix: str, df: pd.DataFrame):
-    start_s = process_time()
-    filename = f"{prefix}.npy"
-    np.save(filename, df.to_numpy())
-    end_s = process_time()
-    return os.path.getsize(filename)/1e6,end_s-start_s
-
-def toFeather(prefix: str, df: pd.DataFrame):
-    start_s = process_time()
-    filename = f"{prefix}.feather"
-    df.to_feather(filename)
-    end_s = process_time()
-    return os.path.getsize(filename)/1e6,end_s-start_s
-
-def toCSV(prefix: str, df: pd.DataFrame):
-    start_s = process_time()
-    filename = f"{prefix}.csv"
-    df.to_csv(filename)
-    end_s = process_time()
-    return os.path.getsize(filename)/1e6,end_s-start_s
+def loadJson(filename: str, post_process=False) -> LogData:
+    # TODO speed this up
+    log_data = LogData()
+    with open(filename, "r") as file:
+        json_obj = json.load(file)
+        if "dataframe" in json_obj:
+            log_data.df = pd.DataFrame(**json_obj["dataframe"])
+        if "header" in json_obj:
+            json_format.ParseDict(json_obj["header"], log_data.header)
+    if post_process:
+        postProcessLogData(log_data)
+    return log_data
 
 
 def dumpLogDataToJson(filename: str, log_data: LogData):
+    # TODO speed this up
     with open(filename, "w") as file:
-        df = log_data.df
-        formats = ["parquet", "json", "pickle", "numpy", "feather", "csv"]
-        stats = []
-        stats.append(toParquet(filename, df))
-        stats.append(toJson(filename, df))
-        stats.append(toPickle(filename, df))
-        stats.append(toNumpy(filename, df))
-        stats.append(toFeather(filename, df))
-        stats.append(toCSV(filename, df))
-
-        #json_data = log_data.df.to_json("out.json", orient="split")
-        #a = json.JSONDecoder().raw_decode(json_data)[0]
-        #json_obj = {
-            #"header": json_format.MessageToJson(log_data.header   ),
-            #"dataframe": a
-        #}
-        #print(type(json_obj["dataframe"]))
-        #file.write(log_data.df.to_json(orient="split"))
-    return formats, stats
+        json_obj = {
+            "header": json.loads(json_format.MessageToJson(log_data.header)),
+            "dataframe": json.loads(log_data.df.to_json(orient="split", index=False)),
+        }
+        json.dump(json_obj, file, separators=(",", ":"))
 
 
-def loadJsonToDataframe(filename: str) -> pd.DataFrame:
-    with open(filename, "r") as file:
-        json_data = file.read()
-        df = pd.read_json(json_data, orient="split")
-
-
-def dumpDataframeToBinary(
-    path: str, header: header_message_pb2.HeaderMessage, df: pd.DataFrame
-) -> None:
-    with open(path, "wb") as bin_file:
-        serialized_header_message = header.SerializeToString()
-        message_type = header_message_id
+def dumpLogDataToBinary(filename: str, log_data: LogData) -> None:
+    with open(filename, "wb") as file:
+        serialized_header_message = log_data.header.SerializeToString()
+        message_id = MessageID.HEADER
         message_length = len(serialized_header_message)
-        delimiter = f"{message_type:01X}{message_length:04X}"
+        delimiter = f"{message_id:01X}{message_length:04X}"
 
-        bin_file.write(delimiter.encode())
-        bin_file.write(serialized_header_message)
+        file.write(delimiter.encode())
+        file.write(serialized_header_message)
 
-        rows = df.to_dict(orient="records")
+        rows = log_data.df.to_dict(orient="records")
         log_message = log_message_pb2.LogMessage()
         for row in rows:
             json_format.ParseDict(row, log_message)
             serialized_log_message = log_message.SerializeToString()
 
-            message_type = log_message_id
+            message_id = MessageID.LOG
             message_length = len(serialized_log_message)
-            delimiter = f"{message_type:01X}{message_length:04X}"
+            delimiter = f"{message_id:01X}{message_length:04X}"
 
-            bin_file.write(delimiter.encode())
-            bin_file.write(serialized_log_message)
-
-
-def dumpFiguresToHTML(
-    figs: List[go.Figure],
-    header: header_message_pb2.HeaderMessage,
-    path: str,
-    offline: bool = False,
-):
-    with open(path, "w") as file:
-        header_info = ""
-        if header != None:
-            header_json = json_format.MessageToJson(header)
-            header_json_formatted = html.escape(header_json).replace("\n", "<br>")
-            header_info = f"<div style='font-size: 28px;'>{header_json_formatted}</div>"
-        file.write(f"<html><head></head><body>{header_info}\n")
-        for fig in figs:
-            tmp_layout = fig.layout
-            fig.update_layout(font={"size": 20})
-            inner_html = (
-                fig.to_html(include_plotlyjs=(True if offline else "cdn"))
-                .split("<body>")[1]
-                .split("</body>")[0]
-            )
-            file.write(inner_html)
-            fig.layout = tmp_layout
-        file.write("</body></html>\n")
+            file.write(delimiter.encode())
+            file.write(serialized_log_message)
 
 
 def appendNormalizedSeries(df: pd.DataFrame) -> None:
@@ -266,55 +189,88 @@ def trimDataframe(
     return df
 
 
-def exportGraphsToHTML(paths, graph_info_file, export_dir="graphs", silent=False):
-    for path in paths:
-        header, df = loadBinary(path)
-        postProcessDataframe(df)
-        figures = []
-        with open(graph_info_file) as file:
-            graph_json_data = json.load(file)
-            for graph_info in graph_json_data["figures"]:
-                if not set(graph_info["y_axis"]).issubset(df.columns):
-                    print(
-                        f'Column(s) Missing: Skipping "{graph_info["title"]}" for {path}'
-                    )
-                    continue
-                traces = [
-                    go.Scatter(x=df[graph_info["x_axis"]], y=df[y_axis], name=y_axis)
-                    for y_axis in graph_info["y_axis"]
-                ]
-                figure = go.Figure(traces)
-                figure.update_layout(
-                    title=graph_info["title"],
-                    xaxis_title=graph_info["x_axis"],
-                    showlegend=True,
-                )
-                figures.append(figure)
-        filename_without_ext = os.path.splitext(os.path.basename(path))[0]
-        html_path = os.path.join(export_dir, f"{filename_without_ext}.html")
-        if not silent:
-            print(f"Exporting {path} -> {html_path}")
-        dumpFiguresToHTML(figures, header, html_path)
-
-
-def exportTuningGraphs(paths, export_path="graphs/tuning.html", silent=False):
-    figures = []
-    for path in paths:
-        header, df = loadBinary(path)
-        postProcessDataframe(df)
-
-        x_axis = "control_cycle_start_s"
-        y_axises = ["engine_rpm", "secondary_rpm", "target_rpm"]
-        title = f"{header.timestamp_human} (KP = {header.p_gain:.06f}, KD = {header.d_gain:.06f})"
-        traces = [
-            go.Scatter(x=df[x_axis], y=df[y_axis], name=y_axis) for y_axis in y_axises
-        ]
-        figure = go.Figure(traces)
-        figure.update_layout(
-            title=title,
-            xaxis_title=x_axis,
-            showlegend=True,
+def figuresToHTML(
+    figures: List[go.Figure],
+    offline: bool = False,
+) -> str:
+    inner_htmls = []
+    for fig in figures:
+        fig.update_layout(font={"size": 20})
+        inner_htmls.append(
+            fig.to_html(include_plotlyjs=(True if offline else "cdn"))
+            .split("<body>")[1]
+            .split("</body>")[0]
         )
-        figures.append(figure)
+    return "".join(inner_htmls)
 
-    dumpFiguresToHTML(figures, None, export_path)
+
+def headerToHTML(header: header_message_pb2.HeaderMessage) -> str:
+    inner_html = "<table border=1>"
+    for field in header_message_pb2.HeaderMessage.DESCRIPTOR.fields:
+        key = field.name
+        value = getattr(header, key)
+        inner_html += f"<tr><td>{key}</td><td>{value}</td></tr>"
+    inner_html += "</table>"
+    return inner_html
+
+
+def dumpLogDataToHTML(filename: str, graph_info_json: dict, log_data: LogData) -> None:
+    figures = createFigures(graph_info_json, log_data)
+    figures_html = figuresToHTML(figures)
+    header_html = headerToHTML(log_data.header)
+    with open(filename, "w") as file:
+        file.write("<html><head></head><body>")
+        file.write(header_html)
+        file.write(figures_html)
+        file.write("</body></html>")
+
+
+def createFigures(graph_info_json: dict, log_data: LogData) -> List[go.Figure]:
+    df = log_data.df
+    figures = []
+    for graph_info in graph_info_json["figures"]:
+        if not set(graph_info["y_axis"]).issubset(df.columns):
+            print(f'Column(s) Missing: Skipping "{graph_info["title"]}"')
+            continue
+
+        traces = [
+            go.Scatter(x=df[graph_info["x_axis"]], y=df[y_axis], name=y_axis)
+            for y_axis in graph_info["y_axis"]
+        ]
+
+        fig = go.Figure(traces)
+        fig.update_layout(
+            title=graph_info["title"],
+            xaxis_title=graph_info["x_axis"],
+            showlegend=True,
+            margin=dict(l=20, r=20, t=60, b=20),
+        )
+        figures.append(fig)
+    return figures
+
+
+# def exportTuningGraphs(paths, export_path="graphs/tuning.html", silent=False):
+#    figures = []
+#    for path in paths:
+#        log_data = loadBinary(path)
+#        postProcessLogData(log_data)
+#
+#        df = log_data.df
+#        header = log_data.header
+#
+#        x_axis = "control_cycle_start_s"
+#        y_axises = ["engine_rpm", "secondary_rpm", "target_rpm"]
+#        title = f"{header.timestamp_human} (KP = {header.p_gain:.06f}, KD = {header.d_gain:.06f})"
+#        traces = [
+#            go.Scatter(x=df[x_axis], y=df[y_axis], name=y_axis) for y_axis in y_axises
+#        ]
+#        figure = go.Figure(traces)
+#        figure.update_layout(
+#            title=title,
+#            xaxis_title=x_axis,
+#            showlegend=True,
+#        )
+#        figures.append(figure)
+#
+#    dumpFiguresToHTML(figures, None, export_path)
+#
